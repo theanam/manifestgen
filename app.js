a6076cfe0954f2aeb7ba20cefa0271ff590f60d1
Anam Ahmed <anam.ahmed.a@gmail.com>
2014-11-14 12:40:38 +0600

    added download option

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 (function(){
 
-	var app = angular.module("moz",['ui.bootstrap']); 
+	var app = angular.module("moz",['ui.bootstrap']);
 	app.controller('AppController',function($scope){
-		
 		this.appd={
 			appname:"hello world",
 			appDescription : "My Elevator Pitch Goes here",
@@ -13,7 +12,16 @@
 			defaultLocale:"en"
 
 		}
-		
+		$scope.downloadManifest = function(){
+			var manifestData = "data:application/text,"+JSON.stringify($scope.appd);
+			console.log(manifestData);
+			var anc = document.createElement('a');
+			anc.href=manifestData.replace(/\s/g,'%20'); //fixed space bug
+			anc.download="manifest.webapp";
+			var evt = document.createEvent("MouseEvents");
+			evt.initMouseEvent("click", true, true, window, 1, 0, 0, 0, 0,false, false, false, false, 0, null);
+			anc.dispatchEvent(evt);
+		}
 		$scope.appd = this.appd;
 		$scope.oneAtATime = true;
 		$scope.status = {
